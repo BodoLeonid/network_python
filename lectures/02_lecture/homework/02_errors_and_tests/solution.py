12431d822e4d2dd0979f2f4246cf13b82c4b1d51
Leonid Bodo <labodo@edu.hse.ru>
2026-07-16 22:27:40 +0300

    hw2 done

---
+++ lectures/02_lecture/homework/02_errors_and_tests/solution.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+if current_dir not in sys.path:
+    sys.path.insert(0, current_dir)
+
+sys.modules.pop("task", None)
+
+from task import app
